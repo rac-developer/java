43a8f347d0102e5911d1ef94c88dd45fa8438008
Rodolfo Castro <soyracm@gmail.com>
2025-09-04 16:00:42 -0400

    testing

--- projects/00-basic/src/Main.java
+++ projects/00-basic/src/Main.java
@@ -1,7 +1,7 @@
 
+
 public class Main {
-    // Metodo principal es un bloque de codigo que se ejecuta al iniciar el programa
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+    };
 }
